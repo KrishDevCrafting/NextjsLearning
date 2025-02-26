5cbdaa1a10cb9885e9df0c576c5e48a45d9444c3
KrishDevCrafting <krishkumer150@gmail.com>
2025-02-26 12:32:07 +0530

    Updated

--- my-app/app/loginPage/page.js
+++ my-app/app/loginPage/page.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function Login() {
   return (
-    <div><h1>Login-here!!</h1></div>
-  )
+    <div>
+      <h1 className="text-5xl text-center">Login-here!!</h1>
+
+      <form className="text-center border border-light p-5">
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter your name"
+          className=" border-2 border-red-700  rounded-md focus:ring-2 focus:ring-blue-500 border border-gray-300 m-4 bg-red-700"
+        />
+        <br></br>
+        <label>Email</label>
+        <input type="email" name="email" placeholder="Enter your email" className="border-2 border-red-700  rounded-md focus:ring-2 focus:ring-blue-500 border border-gray-300 m-4 bg-red-700" />
+        <br></br>
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          className="border-2 border-red-700  rounded-md focus:ring-2 focus:ring-blue-500 border border-gray-300 m-4 bg-red-700"
+          placeholder="Enter your password"
+        />
+        <br></br>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
