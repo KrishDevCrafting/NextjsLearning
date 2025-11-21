30c874f549baa6ffaa1bc3ee8c5e21ba1dd0a0ca
KrishDevCrafting <krishkumer150@gmail.com>
2025-11-21 23:11:40 +0530

    updated

---
+++ my-app/src/app/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="flex items-center justify-between px-6 py-4">
+        <div className="flex-shrink-0">
+          <h1 className="text-2xl font-bold text-blue-600">Logo</h1>
+        </div>
+        <div>
+          <ul className="flex space-x-8">
+            <li className="text-gray-700 hover:text-blue-600 cursor-pointer font-medium transition">
+              Home
+            </li>
+            <li className="text-gray-700 hover:text-blue-600 cursor-pointer font-medium transition">
+              General
+            </li>
+            <li className="text-gray-700 hover:text-blue-600 cursor-pointer font-medium transition">
+              Feedback!
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 export default function Home() {
+  console.log("what type to girl you are!");
+
   return (
     <div>
+      <Header />
       <h1>Hello-World</h1>
     </div>
   );
