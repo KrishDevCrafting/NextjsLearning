9717a044b1c165b149dc659908d0e63338deb1a5
KrishDevCrafting <krishkumer150@gmail.com>
2025-11-25 16:06:41 +0530

    Routing

--- my-app/src/app/dashboard/user/page.tsx
+++ my-app/src/app/dashboard/user/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import Link from "next/link";
 const User = () => {
-  return <div>User</div>;
+  return (
+    <div>
+      <h1>Dashboard User</h1>
+
+      <ul>
+        <li>
+          <Link href="/dashboard/User1">User1</Link>
+        </li>
+        <li>
+          <Link href="dashboard/User2">User2</Link>
+        </li>
+        <li>
+          <Link href="dashboard/User3">User 3</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default User;
