7e2231ae7a529ed0a76a9857bd5da531315199fc
KrishDevCrafting <krishkumer150@gmail.com>
2025-12-29 21:50:49 +0530

    updated

--- dev-project/components/Explorebtn.tsx
+++ dev-project/components/Explorebtn.tsx
@@ -1,15 +1,27 @@
 "use client";
 import React from "react";
-
+import Image from "next/image";
 const Explorebtn = () => {
   return (
     <button
-      className="mt-7 mx-auto"
       id="explore-btn"
-      type="button"
-      onClick={() => alert("Hello Dear!")}
+      role="button"
+      className="mt-7 mx-auto  px-4 py-2 rounded-full  text-black"
     >
-      Explore button
+      <a
+        href="#events"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-white"
+      >
+        Explore button
+        <Image
+          src="/icons/arrow-down.svg"
+          alt="arrow-img"
+          width={24}
+          height={24}
+        />
+      </a>
     </button>
   );
 };
