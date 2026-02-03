277f59822b496b0efff8971ba5be5b6bc12741c6
KrishDevCrafting <krishkumer150@gmail.com>
2026-02-03 15:19:56 +0530

    bookin-action.ts

---
+++ dev-project/lib/actions/booking.action.ts
@@ -0,0 +1,26 @@
+import Booking from "@/app/database/booking.model";
+import connectDB from "../mongoosedb";
+export const createBooking = async ({
+  eventId,
+  slug,
+  email,
+}: {
+  eventId: string;
+  slug: string;
+  email: string;
+}) => {
+  try {
+    await connectDB();
+    const booking = (
+      await Booking.create({
+        eventId,
+        slug,
+        email,
+      })
+    ).lean();
+    return { success: true, booking };
+  } catch (e) {
+    console.error("Create booking failed", e);
+    return { success: false, e: e };
+  }
+};
